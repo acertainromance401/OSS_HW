09187fe2027c58a3224a84a270ddc3de942c008e
JaeHyun Lim <acertainromance401@gmail.com>
2026-05-27 05:25:47 +0900

    Parameter(test_size)변경 실험

--- HW.py
+++ HW.py
@@ -7,7 +7,7 @@ print(linnerud)
 from sklearn.model_selection import train_test_split
 X = linnerud.data
 y = linnerud.target
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.5,random_state=4)
 print(X_train.shape)
 print(X_test.shape)
 
