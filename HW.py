630e6757114c635ef37befecde53cb29744d3a3a
JaeHyun Lim <acertainromance401@gmail.com>
2026-06-03 16:03:00 +0900

    Matplotlib 그래프 추가

--- HW.py
+++ HW.py
@@ -7,7 +7,7 @@ print(linnerud)
 from sklearn.model_selection import train_test_split
 X = linnerud.data
 y = linnerud.target
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.5,random_state=4)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
 print(X_train.shape)
 print(X_test.shape)
 
@@ -26,3 +26,29 @@ print("MSE =", mse)
 x_new = [[10, 50, 100], [9, 120, 150]]
 y_predict = LR.predict(x_new)
 print(y_predict)
+
+import matplotlib.pyplot as plt
+
+target_names = linnerud.target_names
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+for i in range(3):
+    axes[i].scatter(y_test[:, i], y_pred[:, i])
+
+    # 이상적인 예측선
+    min_val = min(y_test[:, i].min(), y_pred[:, i].min())
+    max_val = max(y_test[:, i].max(), y_pred[:, i].max())
+
+    axes[i].plot(
+        [min_val, max_val],
+        [min_val, max_val],
+        'r--'
+    )
+
+    axes[i].set_title(target_names[i])
+    axes[i].set_xlabel("Actual")
+    axes[i].set_ylabel("Predicted")
+
+plt.tight_layout()
+plt.show()
